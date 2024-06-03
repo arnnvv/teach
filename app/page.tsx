f1cd85674cb05e42fd292fc5d4f9e55b9bf21bb6
arnnvv <arnav.900@protonmail.com>
2024-06-03 23:29:57 +0530

    rest

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import Board from "./../components/Board";
+
+export default function Home(): JSX.Element {
+  return <Board />;
+}
---
+++ components/Board.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface BoardInterface {}
+
+const Board: FC<BoardInterface> = ({}: BoardInterface): JSX.Element => {
+  return <>HEY</>;
+};
+
+export default Board;
